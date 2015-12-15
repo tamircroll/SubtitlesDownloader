2b425c07800232c44f71df7af46351aabfe3ac52
tamircroll <tamircroll@gmail>
2015-12-16 00:54:22 +0200

    First Commit

---
+++ ConsoleApplication1/OpenSubtitlesDownloader.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    public class OpenSubtitlesDownloader
+    {
+
+        public string getToken()
+        {
+            WebRequest request = WebRequest.Create(@"http://api.opensubtitles.org:80/xml-rpc");
+            byte[] body = Encoding.UTF8.GetBytes(getTokenXML);
+
+            string responseFromServer = getResponseFromServer(request, body);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(responseFromServer);
+
+            XmlNode firstChild = xmlDoc.GetElementsByTagName("string")[0];
+
+            return firstChild.InnerText;
+        }
+
+        public void searchSubs(string i_Hash, string i_Token, string i_File)
+        {
+            string fileLength = new FileInfo(i_File).Length.ToString();
+            string searchSubsXml = string.Format(serachSubs, i_Token, i_Hash, fileLength);
+
+            WebRequest request = WebRequest.Create(@"http://api.opensubtitles.org:80/xml-rpc");
+            byte[] body = Encoding.UTF8.GetBytes(searchSubsXml);
+            string response = getResponseFromServer(request, body);
+
+            Console.WriteLine(response);
+        }
+
+        private string getResponseFromServer(WebRequest request, byte[] body)
+        {
+            request.Method = "POST";
+            request.ContentType = "text/xml";
+            request.ContentLength = body.Length;
+
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(body, 0, body.Length);
+            dataStream.Close();
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            dataStream = response.GetResponseStream();
+            if (dataStream != null)
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+
+                return responseFromServer;
+            }
+
+            throw new Exception("Couldn't get info from server");
+        }
+
+
+        private string getTokenXML =
+@"<methodCall>
+ <methodName>LogIn</methodName>
+ <params>
+  <param>
+   <value><string>tamircroll</string></value>
+  </param>
+  <param>
+   <value><string>Asdf1234</string></value>
+  </param>
+  <param>
+   <value><string>en</string></value>
+  </param>
+  <param>
+   <value><string>SolEol 0.0.8</string></value>
+  </param>
+ </params>
+</methodCall>";
+        
+        private readonly string serachSubs =
+ @"<methodCall>
+ <methodName>SearchSubtitles</methodName>
+ <params>
+  <param>
+   <value><string>{0}</string></value>
+  </param>
+  <param>
+   <value>
+    <array>
+     <data>
+      <value>
+       <struct>
+        <member>
+         <name>TheSubsDownloader</name>
+         <value><string>heb,eng</string>
+         </value>
+        </member>
+        <member>
+         <name>moviehash</name>
+         <value><string>{1}</string></value>
+        </member>
+        <member>
+         <name>moviebytesize</name>
+         <value><double>{2}</double></value>
+        </member>
+       </struct>
+      </value>
+     </data>
+    </array>
+   </value>
+  </param>
+ </params>
+</methodCall>";
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private List<String> videoExtentions = new List<String>
+        {
+            "*.3g2","*.3gp","*.3gp2","*.3gpp","*.60d","*.ajp","*.asf","*.asx","*.avchd","*.avi","*.bik","*.bix","*.box","*.cam","*.dat","*.divx","*.dmf",
+            "*.dv","*.dvr-ms","*.evo","*.flc","*.fli","*.flic","*.flv","*.flx","*.gvi","*.gvp","*.h264","*.m1v","*.m2p","*.m2ts","*.m2v","*.m4e","*.m4v",
+            "*.mjp","*.mjpeg","*.mjpg","*.mkv","*.moov","*.mov","*.movhd","*.movie","*.movx","*.mp4","*.mpe","*.mpeg","*.mpg","*.mpv","*.mpv2","*.mxf","*.nsv",
+            "*.nut","*.ogg","*.ogm","*.omf","*.ps","*.qt","*.ram","*.rm","*.rmvb","*.swf", "*.ts","*.vfw","*.vid","*.video","*.viv","*.vivo","*.vob","*.vro",
+            "*.wm","*.wmv","*.wmx","*.wrap","*.wvx","*.wx","*.x264","*.xvid"
+        };
+
+        private static void Main(string[] args)
+        {
+            string file = @"C:\Users\dell\Desktop\Movies\The Wire\Season 1\The Wire Season 1 Episode 01 - The Target.avi";
+            byte[] moviehash = HashCoder.ComputeMovieHash( file);
+            string hash = HashCoder.ToHexadecimal(moviehash);
+            OpenSubtitlesDownloader openSubtitlesDownloader = new OpenSubtitlesDownloader();
+
+            string token = openSubtitlesDownloader.getToken();
+            openSubtitlesDownloader.searchSubs(hash, token, file);
+
+            Console.ReadLine();
+        }
+    }
+}
